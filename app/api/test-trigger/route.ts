395fb81c4bdaae86e9f240609444b42ad188129e
Liam Buchanan <l.buchanan358@gmail.com>
2025-10-21 20:07:21 +0100

    fix

--- app/api/test-trigger/route.ts
+++ app/api/test-trigger/route.ts
@@ -1,19 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { tasks } from "@trigger.dev/sdk";
 import { NextResponse } from "next/server";
-import type { testFirecrawl } from "@/src/trigger/test-firecrawl";
+// import type { testFirecrawl } from "@/src/trigger/test-firecrawl";
 
 //tasks.trigger also works with the edge runtime
 //export const runtime = "edge";
 
 export async function GET() {
-  const handle = await tasks.trigger<typeof testFirecrawl>(
-    "test-firecrawl",
-    {
-      // url: "https://www.connectvargroup.io/",
-      url: 'https://zenflow-ai.com/'
-    }
-  );
+  // const handle = await tasks.trigger<typeof testFirecrawl>(
+  //   "test-firecrawl",
+  //   {
+  //     // url: "https://www.connectvargroup.io/",
+  //     url: 'https://zenflow-ai.com/'
+  //   }
+  // );
 
-  return NextResponse.json(handle);
+  return NextResponse.json({ message: "Trigger fired" });
 }
